46eecc0a1d73c9af75aad42c50b519b58b6e90ec
Sarach Griffin <peggi2@hotmail.com>
2020-07-14 21:28:06 +0100

    change to python2

---
+++ yamlConverterpython2.py
@@ -0,0 +1,77 @@
+u"""
+
+Project: FTIRDB
+File: yamlConverter.py
+
+Function: 
+
+This program is released under the GNU Public Licence (GPL V3)
+
+--------------------------------------------------------------------------
+Description:
+
+Script called from command line with name and location of yaml file will convert yaml to tsv file
+
+
+Example 
+============
+
+
+"""
+
+
+
+
+
+
+
+from __future__ import with_statement
+from __future__ import absolute_import
+import csv
+import pandas as pd
+import os
+import yaml
+import pathlib
+import path
+import time
+import sys
+from io import open
+
+#need to consider checking the file input is the correct format
+#need to create tests 
+
+start = time.process_time()
+
+seq = sys.argv[1]
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+test = os.path.join(dirname,u'partis1g2t.yaml')
+
+def openYaml(sequence):
+    with open (sequence, u'r') as f:
+        doc = yaml.safe_load(f)
+    return(doc)
+
+def extractRequired(yamlFile):
+    fieldNames = [u'v', u'd', u'j']
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    txt = yamlFile[u"germline-info"][u"seqs"]
+    requiredFields = {}
+    for field in fieldNames:
+        v = list(txt[field].values())
+        k = field
+        requiredFields[k]=v
+    
+    cdr3 = yamlFile[u"events"][0][u"cdr3_seqs"]
+    requiredFields[u'cdr3'] = cdr3
+    fieldNames.append(u'cdr3')
+    tsvfilename = os.path.join(dirname, u'TSVFile.tsv')
+
+    df = pd.DataFrame(requiredFields)
+    df.to_csv(tsvfilename, sep=u'\t', index=False, header=fieldNames, encoding=u'utf-8')
+    return(u'saved at:  ' + unicode(dirname))
+
+yaml = openYaml(seq)
+savedFile = extractRequired(yaml)
+print savedFile
+print time.process_time()-start
